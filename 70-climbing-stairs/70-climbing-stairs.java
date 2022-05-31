1b345e03e6787e2f71ed754274226f8d84b71512
neha1203 <43581483+neha1203@users.noreply.github.com>
2022-05-31 19:46:47 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (65.75%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] fn = new int[n+1];
+        fn[0] = 1;
+        fn[1] = 1;
+    
+    
+        
+        for( int i = 2 ; i<=n;i++){
+            
+            fn[i] = fn[i-1] + fn[i-2];
+            
+            
+        }
+        
+        
+       return fn[n]; 
+        
+        
+        
+        
+        
+    }
+}
