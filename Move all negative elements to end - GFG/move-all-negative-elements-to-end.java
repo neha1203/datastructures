0f65bf3cc6c861195e60e881eec9c529f55c6185
neha1203 <43581483+neha1203@users.noreply.github.com>
2022-06-11 12:44:59 +0530

    Added solution - LeetHub

--- Move all negative elements to end - GFG/move-all-negative-elements-to-end.java
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.java
@@ -40,40 +40,31 @@ class Solution {
     
     public void segregateElements(int arr[], int n)
     {
-       
-      int temp[]  = new int[n];
-      int j = 0;
-      
-      for( int i = 0 ; i<n;i++)
-      {
-          if(arr[i]> 0)
-          temp[j++] = arr[i];
-          
-          
-      }
-      
-      if( j==n || j==0)
-      return;
-      
-          for( int i = 0 ; i<n;i++)
-      {
-          if(arr[i]< 0)
-          temp[j++] = arr[i];
-          
-          
-      } 
-      
-      
-      for( int i = 0 ; i<n;i++){
-          arr[i] = temp[i];
-          
-          
-          
-          
-          
-          
-          
-      }
+        
+        int j  = 0 ;
+        int[] temp = new int[n];
+        for( int i = 0 ; i<n ; i++){
+            if(arr[i]>0)
+               temp[j++] = arr[i];
+        }  
+            if( j ==0 ||  j == n )
+              return;
+            
+          for( int i = 0 ; i<n ; i++){
+            if(arr[i]<0)
+               temp[j++] = arr[i];
+        }    
+            
+         for( int i = 0 ; i<n ; i++){
+            arr[i] = temp[i];
+        }     
+            
+            
+        
+        
+        
+        
+        
         
         
         // Your code goes here
