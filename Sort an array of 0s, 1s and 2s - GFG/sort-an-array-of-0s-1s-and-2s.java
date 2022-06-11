c1207a1c7ffb56a1a4b0197c6478e05737d014e8
neha1203 <43581483+neha1203@users.noreply.github.com>
2022-06-11 12:11:26 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
@@ -12,34 +12,27 @@ class Solution
 {
     public static void sort012(int a[], int n)
     {
-        int count0 = 0;
+        
         int count1 = 0;
         int count2 = 0;
+        int count3 = 0;
         
-        for( int i = 0 ; i<a.length ; i++){
-            
-            
-            switch(a[i]){
-            
-            case 0:
-                count0++;
-                break;
-                
-            case 1:
-                count1++;
-                break;
-            default :
-               count2++;
-               break;
+    for( int i = 0 ; i<n ; i++){
+        
+        switch (a[i]){
+            case 0 :
+                 count1++;
+                 break;
+            case 1 :
+                  count2++;
+                  break;
+            case 2 :
+                    count3++;
+                    break;
             
             
             
             
-            
-            
-            
-            
-            }   
             
             
             
@@ -47,38 +40,51 @@ class Solution
             
         }
         
-        int i = 0;
-        while(count0> 0){
-            
-         a[i] = 0;
-         count0--;
-         i++;
-            
-            
-        }
-        while(count1> 0){
-            
-         a[i] = 1;
-         count1--;
-         i++;
-            
-            
-        }
-          
-          
-                while(count2> 0){
-            
-         a[i] = 2;
-         count2--;
-         i++;
-            
-            
-        }  
-            
         
         
         
         
+        
+        
+        
+        
+        
+        
+        
+    }
+    int i = 0;
+    
+    while(count1> 0)
+    
+    {
+        
+        a[i] = 0;
+        count1--;
+        i++;
+        
+        
+    }
+    
+        while(count2> 0)
+    
+    {
+        
+        a[i] = 1;
+        count2--;
+        i++;
+        
+        
+    }
+    while(count3> 0)
+    
+    {
+        
+        a[i] = 2;
+        count3--;
+        i++;
+        
+        
+    }
         
         // code here 
     }
