c4e4f25c3fa6239a3acb221b215fe9b7751fac72
neha1203 <43581483+neha1203@users.noreply.github.com>
2022-06-18 11:02:54 +0530

    Added solution - LeetHub

--- Non-Repeating Element - GFG/non-repeating-element.java
+++ Non-Repeating Element - GFG/non-repeating-element.java
@@ -44,39 +44,43 @@ class Check{
     
     public int firstNonRepeating(int arr[], int n) 
     { 
-        LinkedHashMap<Integer,Integer> hs = new LinkedHashMap<>();
+        HashMap<Integer,Integer> hs = new HashMap<Integer,Integer>();
         
-        for( int i = 0 ; i<n ; i++){
+        for( int i = 0 ; i<n;i++){
             
-            if(hs.containsKey(arr[i])){
+            
+            if(!hs.containsKey(arr[i])){
                 
+                hs.put(arr[i],1);
                 
+            }
+            else{
                 
                 hs.put(arr[i],hs.get(arr[i])+1);
+                
             }
-            else
-             hs.put(arr[i],1);
-            
-            
-            
-            
-            
-            
             
         }
+      
+      
+      
       for( int i = 0 ; i<n ; i++){
+          if(hs.get(arr[i]) == 1)
           
-          if(hs.get(arr[i])==1)
-          
-           return arr[i];
-          
-          
+          return arr[i];
           
       }  
+      
+      
+      return 0;
+            
+        }
         
         
-     return 0;   // Complete the function
-    }  
+        
+        
+        // Complete the function
+    
     
 }
 
